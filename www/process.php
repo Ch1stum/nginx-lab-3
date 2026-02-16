d9a28da18ece496a6058f8b3b3b988a3377f257d
Ch1stum <aresteam39@mail.ru>
2026-02-16 22:20:18 +0200

    Изменил форму, создал и настроил process.php под свою форму

---
+++ www/process.php
@@ -0,0 +1,22 @@
+<?php
+// Запускаем сессию (пункт 2 ТЗ)
+session_start();
+
+$username = htmlspecialchars($_POST['username'] ?? '');
+$userage = htmlspecialchars($_POST['userage'] ?? '');
+$email = htmlspecialchars($_POST['email'] ?? '');
+$theme = htmlspecialchars($_POST['theme'] ?? '');
+$materials = isset($_POST['materials']) ? 'Да' : 'Нет';
+$format = htmlspecialchars($_POST['format'] ?? '');
+$date = htmlspecialchars($_POST['date'] ?? '');
+
+$_SESSION['username'] = $username;
+$_SESSION['userage'] = $userage;
+$_SESSION['email'] = $email;
+$_SESSION['theme'] = $theme;
+$_SESSION['materials'] = $materials;
+$_SESSION['format'] = $format;
+$_SESSION['date'] = $date;
+
+header("Location: index.php");
+exit();

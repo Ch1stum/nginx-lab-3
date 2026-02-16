24002cf26a2dcd33240f86ffe9aa8fd62e3513a4
Ch1stum <aresteam39@mail.ru>
2026-02-16 23:16:40 +0200

    Просмотр всех данных + проверка корректности

--- www/process.php
+++ www/process.php
@@ -9,6 +9,17 @@ $materials = isset($_POST['materials']) ? 'Да' : 'Нет';
 $format = htmlspecialchars($_POST['format'] ?? '');
 $date = htmlspecialchars($_POST['date'] ?? '');
 
+$errors = [];
+
+if (empty($username)) $errors[] = "Имя не может быть пустым";
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors[] = "Некорректный email";
+
+if (!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    header("Location: index.php");
+    exit();
+}
+
 $_SESSION['username'] = $username;
 $_SESSION['userage'] = $userage;
 $_SESSION['email'] = $email;
@@ -17,10 +28,8 @@ $_SESSION['materials'] = $materials;
 $_SESSION['format'] = $format;
 $_SESSION['date'] = $date;
 
-
 $line = $username . " " . $email . "\n";
 file_put_contents("data.txt", $line, FILE_APPEND);
-
 
 header("Location: index.php");
 exit();
